8dcce7519c25d96958d00479ec48e83ad595dbe2
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-04-01 02:00:16 +0700

    Menambahkan loop perhitungan bilangan prima

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -20,6 +20,11 @@ void inputAngka() {
 
 bool cekPrima(int n) {
     if (n <= 1) return false;
+    int i = 2;
+    while (i * i <= n) {
+        if (n % i == 0) return false;
+        i++;
+    }
 
 return true;
 }
