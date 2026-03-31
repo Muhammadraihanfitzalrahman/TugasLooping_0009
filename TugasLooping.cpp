76687f920d2f0bc2c10f73d8d112193117669a33
ayamtulebo19@gmail.com <Ayamtulebo19@gmail.com>
2026-04-01 01:53:43 +0700

    Menambahkan isi menu pilihan

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -6,6 +6,12 @@ int pilihan;
 
 void tampilkanMenu() {
     cout << "\n========== MENU UTAMA ==========" << endl;
+    cout << "1. Cek Bilangan Prima" << endl;
+    cout << "2. Cek Bilangan Fibonacci" << endl;
+    cout << "0. Keluar" << endl;
+    cout << "Pilih menu: ";
+    cin >> pilihan;
+
 }
 
 int main() {
